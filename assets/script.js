0e17d08efc76a56a51e0a9179213ac51255907e2
Manohara-Ai <manohara01012005@gmail.com>
2025-11-16 00:14:59 +0530

    add requirements.txt

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// Update progress bar width based on scroll position
+window.onscroll = function() {
+    const winScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.querySelector('.progress-bar').style.width = scrolled + "%";
+};
