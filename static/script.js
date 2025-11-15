4ca8f171bb6ddf3ad564e8eb253444ecc751e543
Manohara <manohara01012005@gmail.com>
2025-11-16 00:17:16 +0530

    Delete static directory

--- static/script.js
+++
@@ -1,7 +0,0 @@
-// Update progress bar width based on scroll position
-window.onscroll = function() {
-    const winScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrolled = (winScroll / height) * 100;
-    document.querySelector('.progress-bar').style.width = scrolled + "%";
-};
